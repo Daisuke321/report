01a1ba689157d149b66220a67de7d2e8edafb29b
Daisuke <coolgangsta36@hotmail.com>
2017-06-09 21:35:02 +0900

    kadai3

--- test17745112ver2.sh
+++ test17745112ver2.sh
@@ -23,17 +23,14 @@ diff $tmp-ans1 $tmp-ten || ERROR_EXIT "TEST1-2"
 # TEST2
 ./17745112.sh 3.5 3 > $tmp-point && ERROR_EXIT "TEST2-1"
 diff $tmp-point $tmp-ans2 || ERROR_EXIT "TEST2-2"
-diff $tmp-point $tmp-ans2 && echo "TEST2 FINE"
 
 # TEST3
 ./17745112.sh 0 0.5 > $tmp-zero && ERROR_EXIT "TEST3-1"
 diff $tmp-zero $tmp-ans3 || ERROR_EXIT "TEST3-2"
-diff $tmp-zero $tmp-ans3 && echo "TEST3 FINE"
 
 # TEST4
 ./17745112.sh 10 15 50 > $tmp-three && ERROR_EXIT "TEST4-1"
 diff $tmp-three $tmp-ans4 || ERROR_EXIT "TEST4-2"
-diff $tmp-three $tmp-ans4 && echo "TEST4 FINE"
 
 # TEST5
 ./17745112.sh 100 50 > $tmp-finish || ERROR_EXIT "TEST5-1"
